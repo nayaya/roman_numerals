d2eb809ec4fcf3a42f0b7b026039ac72fcc88082
ibrahim bashir <bashir_i@qwasar.io>
2020-09-24 14:00:25 +0000

    Fixed function name

--- ex00/my_roman_numerals_converter.rb
+++ ex00/my_roman_numerals_converter.rb
@@ -1,4 +1,4 @@
-def roman_numeral number
+def my_roman_numerals_converter number
     return "The Romans have no zeros/Characters just heros." if number == 0 || !number.is_a?(Integer)
     roman_symbols = {1000 => "M",900 => "CM", 500 => "D",400 => "CD", 100 => "C",90 => "XC", 50 => "L",40 =>"XL", 10 => "X",9 => "IX", 5 => "V",4 => "IV",  1=> "I"}
     symbols = []
